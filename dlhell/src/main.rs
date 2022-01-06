48fc8c16f66d8c95ee7c1cee5ce9579924e36af9
John Agapeyev <jagapeyev@gmail.com>
2022-01-05 20:02:21 -0800

    Fixed signature of return symbol

--- dlhell/src/main.rs
+++ dlhell/src/main.rs
@@ -1,7 +1,7 @@
 fn call_dynamic() {
     unsafe {
         let lib = libloading::Library::new("./target/release/libmylib.so").unwrap();
-        let func: libloading::Symbol<unsafe extern "C" fn() -> u32> = lib.get(b"test").unwrap();
+        let func: libloading::Symbol<unsafe extern "C" fn()> = lib.get(b"test").unwrap();
         func();
     }
 }
