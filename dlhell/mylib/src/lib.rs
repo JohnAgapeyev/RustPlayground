c70a30b46fb7e9c60a8e76a56c7a3f51e3a93524
John Agapeyev <jagapeyev@gmail.com>
2022-01-25 14:47:20 -0800

    More experiments with Rust DLL magic

--- dlhell/mylib/src/lib.rs
+++ dlhell/mylib/src/lib.rs
@@ -1,4 +1,17 @@
 #[no_mangle]
 pub extern "C" fn test() {
-    println!("This is a Rust dynamic library call");
+    println!("This is a Rust dynamic library call using the C ABI");
+}
+
+#[no_mangle]
+pub extern "Rust" fn rtest() {
+    println!("This is a Rust dynamic library call using the Rust ABI");
+}
+
+#[no_mangle]
+pub extern "Rust" fn r2test(v: &mut Vec<u8>) {
+    println!("This is a Rust dynamic library call using the Rust ABI wItH fAnCy ChAnGeS");
+    println!("Wow, this vec has length {}", v.len());
+    v[0] = 77u8;
+    v.push(69u8);
 }
--- dlhell/src/main.rs
+++ dlhell/src/main.rs
@@ -1,8 +1,15 @@
 fn call_dynamic() {
     unsafe {
-        let lib = libloading::Library::new("./target/release/libmylib.so").unwrap();
+        let lib = libloading::Library::new("./target/debug/libmylib.so").unwrap();
         let func: libloading::Symbol<unsafe extern "C" fn()> = lib.get(b"test").unwrap();
         func();
+        let func: libloading::Symbol<unsafe extern "Rust" fn()> = lib.get(b"rtest").unwrap();
+        func();
+        let mut v = vec![0u8; 25];
+        println!("Passing in a Vec of length {} with first element {} and last element {}", v.len(), v[0], v[v.len() - 1]);
+        let func: libloading::Symbol<unsafe extern "Rust" fn(&mut Vec<u8>)> = lib.get(b"r2test").unwrap();
+        func(&mut v);
+        println!("Current have a Vec of length {} with first element {} and last element {}", v.len(), v[0], v[v.len() - 1]);
     }
 }
 
